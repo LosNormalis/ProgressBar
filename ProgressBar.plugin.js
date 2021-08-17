1ec24b074c979f4130e63f9246a4506774d4cf9d
LosNormalis <76221028+LosNormalis@users.noreply.github.com>
2021-08-17 22:54:42 +0200

    Create ProgressBar.plugin.js

---
+++ ProgressBar.plugin.js
@@ -0,0 +1,28 @@
+/**
+ * @name ProgressBar
+ * @author Los_Normalis
+ * @authorId 278543574059057154
+ * @version 1.0.0
+ * @description Add an animated progress bar into zour messages
+ * @invite Jx3TjNS
+ * @donate https://paypal.me/matyasmikus
+ * @source https://github.com/LosNormalis/ProgressBar
+ * @updateUrl https://mwittrien.github.io/BetterDiscordAddons/Plugins/QuickMention/QuickMention.plugin.js
+ */
+
+const config = {
+    info: {
+        name: 'ProgressBar',
+        authors: [
+            {
+                name: 'Los_Normalis',
+                github_username: 'Los_Normalis'
+            }
+        ],
+        version: '1.0.0',
+        description: 'Add an animated progress bar into zour messages',
+        github: 'https://github.com/LosNormalis/ProgressBar',
+        github_raw: ''
+    }
+};
+
